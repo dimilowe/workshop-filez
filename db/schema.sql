826b3d61a9c2258e8988fc88b60f3b68f5a48cee
Dimitrios Lowe <dimitrioslowe@gmail.com>
2025-07-01 09:33:25 -0400

    submission

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,18 @@
 -- TODO
+-- Drop if exists
+DROP TABLE IF EXISTS files;
+DROP TABLE IF EXISTS folders;
+
+-- Create folders table
+CREATE TABLE folders (
+  id SERIAL PRIMARY KEY,
+  name TEXT UNIQUE NOT NULL
+);
+
+-- Create files table
+CREATE TABLE files (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  folder_id INTEGER NOT NULL REFERENCES folders(id) ON DELETE CASCADE,
+  UNIQUE(name, folder_id)
+);
--- db/seed.js
+++ db/seed.js
@@ -7,4 +7,31 @@ console.log("🌱 Database seeded.");
 
 async function seed() {
   // TODO
+  await client.connect();
+
+  await client.query(`DELETE FROM files`);
+  await client.query(`DELETE FROM folders`);
+
+  for (let i = 1; i <= 3; i++) {
+    const folderRes = await client.query(
+      `INSERT INTO folders (name) VALUES ($1) RETURNING *`,
+      [`Folder ${i}`]
+    );
+
+    const folderId = folderRes.rows[0].id;
+
+    for (let j = 1; j <= 5; j++) {
+      await client.query(
+        `INSERT INTO files (name, folder_id) VALUES ($1, $2)`,
+        [`File ${j} of Folder ${i}`, folderId]
+      );
+    }
+  }
+
+  await client.end();
 }
+
+seed().catch((err) => {
+  console.error(err);
+  client.end();
+});
